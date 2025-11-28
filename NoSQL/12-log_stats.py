90fda30848137e0f796e12d54b895d5a88a70e83
ulvialv <ulvialiyevgithub@gmail.com>
2025-11-28 20:23:17 +0000

    asd

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Script that provides some stats about Nginx logs stored in MongoDB """
+
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # HTTP methods stats
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Number of GET /status requests
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
